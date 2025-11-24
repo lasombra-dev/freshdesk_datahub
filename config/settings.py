e354a68f8de3913c7fc9823f58caa311f6bc643a
mentazul <servicios.lasombra@outlook.com>
2025-11-24 11:25:20 -0300

    quitar cosas hardcodeadas

--- config/settings.py
+++ config/settings.py
@@ -5,13 +5,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Configuración de Freshdesk
-FRESHDESK_DOMAIN = os.getenv("FRESHDESK_DOMAIN", "grupoempack.freshdesk.com")
-FRESHDESK_API_KEY = os.getenv("FRESHDESK_API_KEY", "NcWk2UUwZSPC6WCWn3VZ")
-FRESHDESK_START_DATE = os.getenv("FRESHDESK_START_DATE", "2025-10-14")
+FRESHDESK_DOMAIN = os.getenv("FRESHDESK_DOMAIN")
+FRESHDESK_API_KEY = os.getenv("FRESHDESK_API_KEY")
+FRESHDESK_START_DATE = os.getenv("FRESHDESK_START_DATE")
 
 # Configuración de Base de Datos
-DB_SERVER = os.getenv("DB_SERVER", "ge-db-dev02.c904tqksriup.us-east-1.rds.amazonaws.com")
-DB_DATABASE = os.getenv("DB_DATABASE", "freshdesk_datahub")
-DB_USERNAME = os.getenv("DB_USERNAME", "sysdev")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Lg9oAnCywBmKNh")
+DB_SERVER = os.getenv("DB_SERVER")
+DB_DATABASE = os.getenv("DB_DATABASE")
+DB_USERNAME = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DRIVER = os.getenv("DB_DRIVER", "{SQL Server}")
